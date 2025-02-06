4e4dd949ce5420e17b5a6649fe33155522b13c97
Kristina Faye <hellokristinafaye@gmail.com>
2025-02-05 19:37:20 -0800

    first line of animation

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let navbar = document.querySelector('nav');
 
+// this function make the navbar visible when the user scrolls down.
 window.onscroll = function () {
     if (window.scrollY > 0) {
         navbar.style.background = '#eefff9';
