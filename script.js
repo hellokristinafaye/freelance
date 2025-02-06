10d2d08c8ba6c55497f328306475eba617d04d78
Kristina Faye <hellokristinafaye@gmail.com>
2025-02-05 19:27:32 -0800

    some styling and JS to make navbar visible on scroll

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("time");
+let navbar = document.querySelector('nav');
+
+window.onscroll = function () {
+    if (window.scrollY > 0) {
+        navbar.style.background = '#eefff9';
+    } else {
+        navbar.style.background = 'transparent';
+    }
+}
